93516fb6c1b89c2dd2329d3dd0abe2078e091342
Liam Stevens <me@liamstevens.me>
2021-06-12 18:42:03 +1000

    update following considerable work done on server and docker image

--- aws/main.tf
+++ aws/main.tf
@@ -4,21 +4,37 @@ terraform {
             source = "hashicorp/aws"
         }
     }
+    backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "feedback-engineering"
+
+    workspaces {
+      name = "pollinate"
+    }
+  }
 }
+
+
 
 provider "aws" {
     region = var.region
 }
 
 provider "kubernetes" {
-    host                    = module.eks.cluster.endpoint
-    cluster_ca_certificate  = base64decode(module.eks.cluster.certificate_authority.0.data)
-    token                   = module.eks.cluster.token
-    load_config_file        = false
-    version                 = "~> 1.9"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
 
 data "aws_availability_zones" "available" {
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
 }
 
 locals {
@@ -48,6 +64,8 @@ module "vpc" {
     "kubernetes.io/role/internal-elb"             = "1"
   }
 }
+
+
 
 
 module "eks" {
--- deploy/docker/server.py
+++ deploy/docker/server.py
@@ -3,12 +3,17 @@ import logging
 import boto3
 import datetime
 import os
+from get_docker_secret import get_docker_secret
 
 
-session = boto3.session(aws_access_key_id=os.environ['aws_id'], aws_secret_access_key=os.environ['aws_key'])
-client = session.client("dynamodb",)
+
 
 class S(BaseHTTPRequestHandler):
+    access_id = "AKIA2ZEQBPABD2OILGOT"
+
+    access_key = "31+LaS+k87Lou4OwESHonriLmqToiA+ZEr8YJpT2"
+    session = boto3.session.Session(aws_access_key_id=access_id, aws_secret_access_key=access_key)
+    client = session.client("dynamodb",region_name="ap-southeast-2")
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -16,16 +21,25 @@ class S(BaseHTTPRequestHandler):
 
 
     def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
         try:
-            client.put_item(TableName='connections',Item={'nodeID':{'S':'0'},'connectionTime':{'S':datetime.now.strftime("%d/%m/%Y %H:%M:%S")}})
-
+            print("putting data")
+            logging.info('Putting data...\n')
+            self.client.put_item(TableName='connections',Item={'nodeID':{'S':'0'},'connectionTime':{'S':datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}})
+            print("put data.")
+            logging.info('Put data.\n')
+        except Exception as e:
+            logging.info(f"Could not put data: {e}\n")
+            pass
 
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
+    logging.info(f"Starting httpd on {server_address}...\n")
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
